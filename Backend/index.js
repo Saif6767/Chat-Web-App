2dc6e105839c33d93ac31a1c507df95ff4beaa43
Saif6767 <saifk000000002@gamil.com>
2025-05-29 15:09:15 +0530

    Initial commit

---
+++ Backend/index.js
@@ -0,0 +1,29 @@
+import express from "express";
+import authRoutes from "./src/routes/auth.route.js";
+import dotenv from 'dotenv';
+import cors from 'cors';
+import { connectDB } from "./src/lib/db.js";
+import cookieParser from "cookie-parser";
+import messageRoutes from './src/routes/message.route.js';
+import { app, server } from "./src/lib/socket.js";
+
+dotenv.config();
+
+
+const PORT = process.env.PORT;
+
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors({
+    "origin":"http://localhost:5173",
+    credentials: true
+}))
+
+app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
+
+
+server.listen(PORT, () =>{
+    console.log("Server is running on PORT:" + PORT);
+    connectDB()
+});
