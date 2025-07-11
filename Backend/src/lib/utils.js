ade8565451b0b0c743764eb07b8e6defd8b78cc6
Saif Ali Khan <151372458+Saif6767@users.noreply.github.com>
2025-07-11 10:14:46 +0530

    Update utils.js

--- Backend/src/lib/utils.js
+++ Backend/src/lib/utils.js
@@ -9,8 +9,8 @@ export const genrateToken = (userId, res) =>{
     res.cookie("jwt", token, {
         maxAge: 7 * 24 * 60 * 1000,
         httpOnly: true,
-        sameSite: "strict",
-        secure: process.env.NODE_ENV !== "development"
+        sameSite: "None",
+        secure: true
     });
 
     return token;
