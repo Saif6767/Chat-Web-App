8cae9bee0769d355b2491c6870f84e6a2093a4c8
Saif Ali Khan <151372458+Saif6767@users.noreply.github.com>
2025-07-11 09:58:55 +0530

    Update socket.js

--- Backend/src/lib/socket.js
+++ Backend/src/lib/socket.js
@@ -7,7 +7,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: ["https://chatrise.netlify.app/"],
+    origin: ["https://chatrise.netlify.app"],
   },
 });
 
